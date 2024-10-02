e93b1fb5f97fc2bde9a2ad5f2026e8fd192d965b
Fabiano <contatofasantiago@hotmail.com>
2024-10-02 09:11:24 -0300

    start

---
+++ NOC_ContractualConnectivityTest/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fjstavares
+ * Date: 02/10/2024
+ * Time: 09:01
+ */
+using System;
+
+namespace NOC_ContractualConnectivityTest
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
